16b4ddf5a1665a27b9c9e1d7246a31bb4b07124b
IliaSky <iliaskywalker@gmail.com>
2014-10-01 23:29:05 +0300

    Add filter syntax

--- sky_generator.js
+++ sky_generator.js
@@ -1,37 +1,51 @@
 //============================================================================
 //
-//            Sky Generator 1.2 - an HTML generator inspired by Emmet
-//                                  Made by IliaSky
-//           skysite.free.bg     www.github.com/IliaSky     si1.free.bg
+//            Sky Generator 1.3 - an HTML generator inspired by Emmet
+//                             Made by IliaSky
+//              iliasky.com                       github.com/IliaSky
 //
 //============================================================================
 //
-//                           INSTALL INSTRUCTIONS
-// In this order:
-//   1. <script type="text/javascript" src="sky_generator.js"></script>
-//   2. Write your SG.VIEWS patterns
-//   3. Enjoy your SG(pattern, object)
-//   Note: even without step 2 you can use SG.wrap(html, selector)
+//                           USAGE INSTRUCTIONS
 //
+// This file will export the global variable SG
+// API:
+//   SG(pattern, object)
+//   SG.wrap(html, selector)
+//
+// Config:
+//   SG.VIEWS -> your views (patterns or templates)
+//   SG.FLAGS -> flags allow minor conditional logic inside the
+//   SG.FILTERS ->
+//     you can apply them when interpolating attributes and switching objects
+//       by default: 'default' is used when interpolating object attributes
+//                  and 'safe' is used when switching objects
+//
+// Although it is not recommended you can also alter:
+//   SG.REGEX -> regexes used in the script - alter at your own risk
+//   SG.SELF_CLOSING_TAGS and SG.BOOLEAN_ATTRIBUTES -> change them if you want
 //============================================================================
 //
 //                           PATTERN INSTRUCTIONS
 //
 // $.VIEW = inserts view pattern from SG.VIEWS
 // tag#id.class
-  // #this.will.be.a.div.if.there.is.no.tag
+//   #this.will.be.a.div.if.there.is.no.tag
 // tag + tag
 // element[text without html elements]
-  // span[This content will be displayed in a span tag]
-  // note[text inside square brackets is the only place where space matters]
-  // [if tag, id and class are all missing then the text will be in a text node]
+//   span[This content will be displayed in a span tag]
+//   note[text inside square brackets is the only place where space matters]
+//   [if tag, id and class are all missing then this will be in a text node]
 // tag(attribute:value)         tag(attr:val, attr:val)
-  // a(href:#)[click me] + input(type: number, value: 5)
+//   a(href:#)[click me] + input(type: number, value: 5)
 // outer-tag > inner-tag      outer{ inner + inner }
-  // section{ h1[title] + atricle[content] }
+//   section{ h1[title] + atricle[content] }
+//
 // @var = inserts variable from current data object
+// @@ = inserts the value of the current object
 // @(var){markup} = changes current object to object.var inside the brackets
 // ?(flag){markup} = renders markup if flag is on
+// @(var|f){}, @var|f, @@|f = Also applies the filter f from SG.FILTERS
 // *markup = repeats the markup for each element in the current object (array)
 //
 //============================================================================
@@ -45,7 +59,7 @@ SG.VIEWS = {
 
   STORE : "a.store(href:#info/@id)[@title]",
   CATEGORY : "a.category(href:#category-info/@id,title:@name)[@name]",
-  ACTOR : "a.actor(href:#actor-info/@id,title:@firstName_@lastName)[@firstName @lastName]",
+  ACTOR : "a.actor(href:#actor-info/@id)[@firstName @lastName]",
   MOVIE : "h1{a.movie(href:#movie-info/@id)[@title]} + span.date[from @publishDate] +$.RENT_RETURN +article[@description]",
 
   STORES : "section#stores > h1[Stores] + nav > *$.STORE",
@@ -93,10 +107,10 @@ if (!Array.prototype.map) {
 }
 
 SG = function (str, obj){
-  var i, m, n, unclosedBrackets=0;
+  var i, m, n, unclosedBrackets = 0;
 
   // Change a>b+c>d+e to a{b+c{d+e}}
-  str = deconstify(str);
+  str = expandInlineTemplates(str);
   str = greaterThanToBrackets(str);
 
   // Remove all whitespace except for spaces inside []
@@ -107,47 +121,63 @@ SG = function (str, obj){
     return obj.map(function(x){ return SG(str.slice(1), x); }).join('');
 
   // render both sides of '+', not inside {} using recursion
-  for(i=0, n=str.length; i <n ; i++){
-    unclosedBrackets += (str.charAt(i)=='{') - (str.charAt(i)=='}');
-    if (unclosedBrackets===0 && str.charAt(i)=='+')
-      return SG(str.slice(0,i), obj) + SG(str.slice(i+1), obj);
+  for(i = 0, n = str.length; i < n; i++){
+    unclosedBrackets += (str.charAt(i) == '{') - (str.charAt(i) == '}');
+    if (unclosedBrackets === 0 && str.charAt(i) == '+')
+      return SG(str.slice(0, i), obj) + SG(str.slice(i + 1), obj);
   }
 
-  // Change current object to object.innerObject when @(innerObject){}
-  if (m = str.match(/^@\(([_a-z]+)\)\{(.*)\}/i))
-    return obj[m[1]] ? SG(m[2],obj[m[1]]) : '';
+  // @(inner){} => Change current object to obj.inner
+  // @(inner|filter){} => Also apply filter if there is such
+  if (m = str.match(SG.REGEX.CONTEXT_CHANGE))
+    return obj ? SG(m[3], SG.FILTERS[m[2] || 'safe'](obj[m[1]])) : ''; // maybe change this behaviour?
 
-  // Render only when SG.FLAGS[flag] is true when ?(flag){}
-  if (m = str.match(/^\?\(([_a-z]+)\)\{(.*)\}/i))
-    return SG.FLAGS[m[1]] ? SG(m[2],obj) : '';
+  // ?(flag){} => Render only when SG.FLAGS[flag] is true when
+  if (m = str.match(SG.REGEX.FLAG))
+    return SG.FLAGS[m[1]] ? SG(m[2], obj) : '';
 
   // render simple selector and content
   m = str.match(SG.REGEX.ELEMENT);
-  var selector = m[1];
-  var text     = (m[2] || '') + (m[3] ? SG(m[3], obj) : '');
-  var value = SG.wrap(text, selector);
 
-  // change @var with object.var
-  for (i in obj)
-    value = value.replace(RegExp('@'+i,'gi'), obj[i]);
-
-  // change @@ with object
-  return value.replace(/@@/gi, obj);
+  // replace @var with object.var and @var|f with SG.FILTERS[f](object.var)
+  var selector = insertVariableValues(m[1], obj);
+  var textNode = m[2] && insertVariableValues(m[2], obj) || '';
+  var innerElements = m[3] && SG(m[3], obj) || '';
+  return SG.wrap(textNode + innerElements, selector);
 };
+
 SG.VIEWS = {};
 SG.FLAGS = {};
+SG.FILTERS = {
+  stringify: function(e){
+    return JSON.stringify(e);
+  },
+  escape: function(string) {
+    var pre = document.createElement('pre');
+    pre.appendChild(document.createTextNode(string));
+    return pre.innerHTML;
+  },
+  safe: function(string){
+    return string;
+  }
+};
+SG.FILTERS['default'] = SG.FILTERS.escape;
 SG.SELF_CLOSING_TAGS = "area base br col hr img input link meta param command keygen source".split(' ');
 SG.BOOLEAN_ATTRIBUTES = "async autofocus autoplay checked controls default defer disabled formnovalidate hidden ismap loop multiple muted novalidate open readonly required reversed scoped seamless selected truespeed typemustmatch".split(' ');
 
 SG.REGEX = {
-  SIMPLE_SELECTOR : /([?=\-_a-zA-Z0-9@.#(:\/,)$]*)/,
-  INNER_ELEMENTS  : /(?:\{([?=\-_a-zA-Z0-9.#(:,)@$\[\] +*{}\/]*)\})?/,
-  TEXT_NODE       : /(?:\[([\-_a-zA-Z0-9@$ ]*)\])?/,
-
   TAG   : /^([\-_a-zA-Z0-9$]*)/,
   ID    : /(?:#([\-_a-zA-Z0-9$]*))?/,
   CLASS : /([\-._a-zA-Z0-9$]*)/,
-  ATTR  : /(?:\(([?=\/\-_a-zA-Z0-9@$:,#\/]*)\))?/
+  ATTR  : /(?:\(([?=&\.\/\-_a-zA-Z0-9@\|$:,#\/]*)\))?/,
+
+  SIMPLE_SELECTOR : /([?=\-_a-zA-Z0-9@\|.#(:\/,)$]*)/,
+  INNER_ELEMENTS  : /(?:\{([?=\-_a-zA-Z0-9.#(:,)@\|$\[\] +*{}\/]*)\})?/,
+  TEXT_NODE       : /(?:\[([\-_a-zA-Z0-9@\|$ ]*)\])?/,
+
+  CONTEXT_CHANGE : /^@\(([_a-z]+)(?:\|([_a-z]+))?\)\{(.*)\}/i,
+  FLAG           : /^\?\(([_a-z]+)\)\{(.*)\}/i,
+  VARIABLE       : /@(@|[_a-z]+)(?:\|([_a-z]+))?/gi
 };
 var regexConcat = function (arr){
   return RegExp(arr.map(function(e){return SG.REGEX[e].source;}).join(''));
@@ -156,20 +186,24 @@ SG.REGEX.SELECTOR = regexConcat(['TAG', 'ID', 'CLASS', 'ATTR']);
 SG.REGEX.ELEMENT = regexConcat(['SIMPLE_SELECTOR', 'TEXT_NODE', 'INNER_ELEMENTS']);
 
 var greaterThanToBrackets = function (str){
-  return str.split('>').join('{')+ new Array(str.split('>').length).join('}');
+  return str.split('>').join('{') + new Array(str.split('>').length).join('}');
 };
-var deconstify = function (str){
+var expandInlineTemplates = function (str){
   for (var i in SG.VIEWS){
-    var regex = RegExp('\\$\\.'+i+'(?=([^_a-z]|$))','gi');
+    var regex = RegExp('\\$\\.' + i + '(?=([^_a-z]|$))','gi');
     if (str.match(regex))
-      str = str.replace(regex, deconstify(greaterThanToBrackets(SG.VIEWS[i])));
+      str = str.replace(regex, expandInlineTemplates(greaterThanToBrackets(SG.VIEWS[i])));
   }
   return str;
 };
-
-var attr = function (key,value){
+var insertVariableValues = function (str, obj){
+  return str.replace(SG.REGEX.VARIABLE, function(_, key, filter){
+    return SG.FILTERS[filter || 'default'](key == '@' ? obj : obj[key]);
+  });
+};
+var attr = function (key, value){
   if (value)
-    return ' ' + key + '="' + value + '"';
+    return ' ' + key.replace(/"/g, '') + '="' + value.replace(/"/g, '\"') + '"';
   if (SG.BOOLEAN_ATTRIBUTES.indexOf(key) != -1)
     return ' ' + key;
   return '';
